30a85fefe680d9ef7ed40f339fafb23d2a48ec9a
ezigus <eric@zig.us>
2024-02-25 16:38:53 -0500

    added dependencies in requirements - small changes in podcastepisode

--- tests/podcastepisode_test.py
+++ tests/podcastepisode_test.py
@@ -1,16 +1,8 @@
 import pytest
+from email.utils import parsedate_tz, mktime_tz
 from zpodcast.podcastepisode import PodcastEpisode
 
-def test_podcastepisode():
-    # Create a podcast episode object
-    episode = PodcastEpisode(title="Episode 1", description="Episode 1 description", audio_url="https://example.com/episode1.mp3", duration=1800)
-
-    # Test the attributes of the podcast episode
-    assert episode.title == "Episode 1"
-    assert episode.duration == 1800
-    assert episode.description == "Episode 1 description"
-    assert episode.audio_url == "https://example.com/episode1.mp3"
-    
+   
 def test_podcastepisode_title():
     # Create a podcast episode object
     episode = PodcastEpisode(title="Episode 1", description="Episode 1 description", audio_url="https://example.com/episode1.mp3", duration=1800)
@@ -24,3 +16,19 @@ def test_podcastepisode_duration():
 
     # Test the duration attribute of the podcast episode
     assert episode.duration == 1800
+    
+def test_podcastepisode_pub_date_none():
+    # Create a podcast episode object
+    episode = PodcastEpisode(title="Episode 1", description="Episode 1 description", audio_url="https://example.com/episode1.mp3", duration=1800)
+
+    # Test the publication date attribute of the podcast episode
+    assert episode.pub_date == None
+    
+def test_podcastepisode_pub_date():
+    # Create a podcast episode object
+    episode = PodcastEpisode(title="Episode 1", description="Episode 1 description", audio_url="https://example.com/episode1.mp3", duration=1800, pub_date="2021-01-01")
+
+    # Test the publication date attribute of the podcast episode
+    assert episode.pub_date == parsedate_tz("2021-01-01")
+    
+    
